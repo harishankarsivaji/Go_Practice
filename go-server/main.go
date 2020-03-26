5fff3a8a350fba3f178d984b7c497af2593196ea
harishankarsivaji <harishs92@gmail.com>
2020-03-26 14:42:59 +0000

    go modules

--- go-server/main.go
+++ go-server/main.go
@@ -1,15 +1,20 @@
 package main
-import(
-        "fmt"
-        "net/http"
-        "github.com/gorilla/mux"
-        "time"
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
+
 func main() {
-        r := mux.NewRouter()
-        r.HandleFunc("/time", GetTime)
-        http.ListenAndServe(":8000", r)
+	r := mux.NewRouter()
+	r.HandleFunc("/time", GetTime)
+	http.ListenAndServe(":8000", r)
 }
+
+//GetTime function
 func GetTime(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Current time is %s\n", time.Now())
+	fmt.printf(w, "Current time is %s\n", time.Now())
 }
