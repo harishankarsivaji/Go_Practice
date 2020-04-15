f5a1b7460f6805af73fb390f5d6c52ca5c54cd48
harishankarsivaji <harishs92@gmail.com>
2020-04-15 17:54:50 +0100

    testing mux

--- go-server/main.go
+++ go-server/main.go
@@ -1,33 +1,36 @@
 package main
 
 import (
-	// "fmt"
-	// "net/http"
-	// "time"
+	"fmt"
+	"net/http"
+	"time"
 
-	"github.com/gin-gonic/gin"
-	// "github.com/gorilla/mux"
+	// "github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	// Using Gin
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "I am using Gin !!",
-		})
-	})
-	r.Run()
-}
+// func main() {
+// Using Gin
+// 	r := gin.Default()
+// 	r.GET("/ping", func(c *gin.Context) {
+// 		c.JSON(200, gin.H{
+// 			"message": "I am using Gin !!",
+// 		})
+// 	})
+// 	r.Run()
+// }
 
 // // Using Gorilla-mux
-// func main() {
-// 	r := mux.NewRouter()
-// 	r.HandleFunc("/time", GetTime)
-// 	http.ListenAndServe(":8000", r)
-// }
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/time", GetTime)
+	http.ListenAndServe(":8000", r)
+}
 
-// //GetTime function
-// func GetTime(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Current time is %s", time.Now())
-// }
+//GetTime function
+func GetTime(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+	// fmt.Println(r.Header)
+	// fmt.Println(r.Proto)
+	fmt.Fprintf(w, "Current time is %s", time.Now())
+}
