577aa31e3690c10496bf0c966d37efc8961d55df
harishankarsivaji <harishs92@gmail.com>
2020-04-23 18:12:00 +0100

    goroutine using Mutex

--- goroutines/main.go
+++ goroutines/main.go
@@ -6,14 +6,40 @@ import (
 )
 
 var wg = sync.WaitGroup{}
+var m = sync.RWMutex{}
+var counter = 0
 
 func main() {
-	var msg = "Hello"
-	wg.Add(1)
-	go func(msg string) {
-		fmt.Println(msg)
-		wg.Done()
-	}(msg)
-	msg = "Goodbye"
+	// runtime.GOMAXPROCS(1)
+	for i := 0; i < 10; i++ {
+		wg.Add(2)
+		m.RLock()
+		go sayHello()
+		m.Lock()
+		go increment()
+	}
 	wg.Wait()
 }
+
+func sayHello() {
+	fmt.Printf("Hello Go #%v\n", counter)
+	m.RUnlock()
+	wg.Done()
+}
+
+func increment() {
+	counter++
+	m.Unlock()
+	wg.Done()
+}
+
+// func main() {
+// 	var msg = "Hello"
+// 	wg.Add(1)
+// 	go func(msg string) {
+// 		fmt.Println(msg)
+// 		wg.Done()
+// 	}(msg)
+// 	msg = "Goodbye"
+// 	wg.Wait()
+// }
