197b2e3de4b02bb19df2f6535462dab99c67f8c5
harishankarsivaji <harishs92@gmail.com>
2020-04-28 14:55:39 +0100

    channel usinng select statement

--- channels/select-statements/main.go
+++ channels/select-statements/main.go
@@ -23,21 +23,32 @@ type logEntry struct {
 var wg = sync.WaitGroup{}
 var logCh = make(chan logEntry, 50)
 
+//signal-only channel
+var doneCh = make(chan struct{})
+
 func main() {
 	go logger()
 	wg.Add(3)
 	//write log to the channel
 	logCh <- logEntry{time.Now(), logInfo, "Application is starting \n"}
-
 	logCh <- logEntry{time.Now(), logWarning, "Warning message \n"}
-
 	logCh <- logEntry{time.Now(), logError, "Application is shuting down"}
+
 	wg.Wait()
+
+	// shuting down the logger from doneCh - Terminate channel
+	doneCh <- struct{}{}
 }
 
 func logger() {
-	for entry := range logCh {
-		fmt.Printf("%v - [%v] - %v ", entry.time.Format("2006-01-02T15:04:05"), entry.severity, entry.message)
+	for {
+		//select statement
+		select {
+		case entry := <-logCh:
+			fmt.Printf("%v - [%v] - %v ", entry.time.Format("2006-01-02T15:04:05"), entry.severity, entry.message)
+		case <-doneCh:
+			break
+		}
 		wg.Done()
 	}
 }
